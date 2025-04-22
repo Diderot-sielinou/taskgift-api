73542031ce7b2aa9488637005f4bf9624fe38546
diderot-sielinou <diderotsielinou@gmail.com>
2025-04-22 05:04:33 +0100

    nitial commit and added graceful shutdown for the server and configured winston logger Implamentation of db configuration

---
+++ config/db.js
@@ -0,0 +1,110 @@
+import pg from "pg";
+import logger from "../utils/logger.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pg;
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, NODE_ENV } =
+  process.env;
+
+if (!DB_HOST || !DB_PASSWORD || !DB_NAME || !DB_USER || !DB_PORT) {
+  logger.error(
+    "Database environment variables are missing! Check your .env file."
+  );
+  process.exit(1);
+}
+
+const pool = new Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: parseInt(DB_PORT, 10),
+  connectionTimeoutMillis: 200,
+});
+
+logger.info(`database is configured for ${DB_NAME}`);
+pool.on("connect", (client) => {
+  logger.info(`clent connected from `);
+});
+
+pool.on("error", (err, client) => {
+  logger.error(`Unexpected error on idle client in pool ${err}`);
+  process.exit(-1);
+});
+
+const initialzeDbSchema = async () => {
+  const client = await pool.connect();
+  try {
+    logger.info(`Initializing data schema...`);
+    await client.query("CREATE EXTENSION IF NOT EXISTS pgcrypto")
+
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        profile_image_url VARCHAR(255),
+        created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updated_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+      );
+      `);
+    logger.info("users table has been created");
+
+    await client.query(`
+        CREATE TABLE IF NOT EXISTS tasks (
+          id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+          owner_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+          title VARCHAR(255) NOT NULL,
+          description TEXT,
+          completed BOOLEAN DEFAULT FALSE,
+          due_date DATE,
+          created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+          updated_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+        );
+      `);
+
+    logger.info("Tasks table has been created");
+    await client.query(
+      "CREATE INDEX IF NOT EXISTS idx_users_email ON users(email)"
+    );
+    await client.query(
+      "CREATE INDEX IF NOT EXISTS idx_tasks_user_id ON tasks(owner_id)"
+    );
+    logger.info("index have been created")
+  } catch (error) {
+    logger.error(`Error while Initializing the schema`,error)
+  }finally{
+    client.release
+  }
+};
+
+
+const connectToDb = async () => {
+  try {
+    const client = await pool.connect()
+    logger.info(`Database connection pool established successfully`)
+    client.release()
+  } catch (error) {
+    logger.error('Unable to establish database connection pool', error)
+    process.exit(1)
+  }
+}
+
+const query = async (text, params) => {
+  const start = Date.now()
+  try {
+    const response = await pool.query(text, params)
+    const duration = Date.now() - start;
+    logger.info(`Executed query: { text: ${text.substring(0, 100)}..., params: ${JSON.stringify(params)}, duration: ${duration}ms, rows: ${response.rowCount}}`);
+    return response
+  } catch (error) {
+    logger.error(`Error executing query: { text: ${text.substring(0, 100)}..., params: ${JSON.stringify(params)}, error: ${error.message}}`);
+    throw error
+  }
+}
+
+export { pool, connectToDb, query, initialzeDbSchema }
